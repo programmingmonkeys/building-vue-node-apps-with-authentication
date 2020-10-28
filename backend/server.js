1bac5685d61810169467bfca868f33e48a722b83
codingChewie <codingChewie@gmail.com>
2020-10-28 11:23:28 -0500

    creating a post service

--- backend/server.js
+++ backend/server.js
@@ -3,11 +3,17 @@ const cors = require('cors')
 const app = express()
 const port = 3000
 
+app.use(express.json())
 app.use(cors())
 
 app.get('/messages', (req, res) => {
   const messages = ['hello', 'hi', 'its working', 'each']
   res.send(messages)
+})
+
+app.post('/messages', (req, res) => {
+  console.log(req.body)
+  res.json({})
 })
 
 app.listen(port, () => console.log('app running'))
