2f73e507d4b3cd2f9717aedaa63d03baef9cd4a4
codingChewie <codingChewie@gmail.com>
2020-10-28 11:16:21 -0500

    updating your frontend to get data from service

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,12 @@
 const express = require('express')
-
+const cors = require('cors')
 const app = express()
 const port = 3000
 
+app.use(cors())
+
 app.get('/messages', (req, res) => {
-  const messages = ['hello', 'hi', 'its working']
+  const messages = ['hello', 'hi', 'its working', 'each']
   res.send(messages)
 })
 
--- frontend/src/components/Messages.vue
+++ frontend/src/components/Messages.vue
@@ -1,22 +1,23 @@
 <template>
   <div>
     <h4 class="display-1">Messages</h4>
-    <v-list>
-      <v-list-tile v-for="(message, i) in messages" :key="i">
-        <v-list-tile-content>
-          <v-list-tile-title v-text="message"></v-list-tile-title>
-        </v-list-tile-content>
-      </v-list-tile>
-    </v-list>
+    <ul v-for="(message, i) in messages" :key="i">
+      <li :key="i">{{ message }}</li>
+    </ul>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
   data() {
     return {
-      messages: ['hello', 'hi', 'its working'],
+      messages: [],
     }
+  },
+  async created() {
+    this.messages = (await axios.get('http://localhost:3000/messages')).data
   },
 }
 </script>
